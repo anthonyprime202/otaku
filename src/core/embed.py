9ae5b8b0a269f2ca7b6e7727cd928559a8bad7a5
anthonyprime202 <anthonyprime202@gmail.com>
2023-04-27 20:51:54 +0530

    Added current time property

--- src/core/embed.py
+++ src/core/embed.py
@@ -1,15 +1,22 @@
 import discord
 import yaml
 
-from .helpers import DictToObject
+from .utils import DictToObject
+
+from datetime import datetime
 
 class Embed(discord.Embed):
     @property
     def colors(self):
-        with open("./config.yml", mode="r") as config:
+        with open("./config.yaml", mode="r") as config:
             data = DictToObject(yaml.load(config, yaml.FullLoader)["color"])
         return data
     
     @property
     def colours(self):
         return self.colors
+    
+    @property
+    def current_time(self) -> datetime:
+        """Returns the current utc timestamp"""
+        return discord.utils.utcnow()

af12a2a86b8e8e4bc6795cbce11409a236b3829a
anthonyprime202 <anthonyprime202@gmail.com>
2023-04-27 21:22:24 +0530

    Added select query for completed giveaways

--- src/extensions/giveaway/queries.py
+++ src/extensions/giveaway/queries.py
@@ -11,6 +11,11 @@ WHERE completed = FALSE
 ORDER BY end_time ASC
 """
 
+select_completed_giveaway = """
+SELECT giveaway_id, end_time
+WHERE completed = TRUE
+"""
+
 insert_giveaway = """
 INSERT INTO giveaways (giveaway_id, channel_id, guild_id, host, prize, description, start_time, end_time)
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
